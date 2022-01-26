92dbcbc157eed8dd6c921db001c858a29b7a029c
ashkan mogharab <ashkanmogharab77@aut.ac.ir>
2022-01-26 14:36:57 +0330

    TimelineController added to project

---
+++ src/main/java/com/example/clientgui/TimelineController.java
@@ -0,0 +1,369 @@
+package com.example.clientgui;
+
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.Scanner;
+
+public class TimelineController implements Initializable {
+    String username;
+    usefulMethods usefulmethods = new usefulMethods();
+    @FXML
+    private VBox TimelineArea;
+    @FXML
+    private ScrollPane scrollPane;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    @FXML
+    void shortCutKey(KeyEvent event) {
+        try {
+            if (event.getCode() == KeyCode.D) {
+                if (scrollPane.getVvalue() != scrollPane.getVmax()) {
+                    scrollPane.setVvalue(scrollPane.getVvalue() + 50.0);
+                }
+            } else if (event.getCode() == KeyCode.U) {
+                if (scrollPane.getVvalue() != 0.0) {
+                    scrollPane.setVvalue(scrollPane.getVvalue() - 50.0);
+                }
+            } else if (event.getCode() == KeyCode.R) {
+                if (scrollPane.getHvalue() != scrollPane.getHmax()) {
+                    scrollPane.setHvalue(scrollPane.getHvalue() + 1.0);
+                }
+            } else if (event.getCode() == KeyCode.L) {
+                if (scrollPane.getHvalue() != 0) {
+                    scrollPane.setHvalue(scrollPane.getHvalue() - 1.0);
+                }
+            } else if (event.getCode() == KeyCode.F2) {
+                viewSearch(event);
+            } else if (event.getCode() == KeyCode.F3) {
+                viewProfile(event);
+            } else if (event.getCode() == KeyCode.F4) {
+                showTweetScene(event);
+            } else if (event.getCode() == KeyCode.F5) {
+                comeToTimeline();
+            }
+        } catch (IOException | InterruptedException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @FXML
+    void viewSearch1(ActionEvent event) throws IOException {
+        viewSearch(event);
+    }
+
+    @FXML
+    void showTweetScene1(ActionEvent event) throws IOException {
+        showTweetScene(event);
+    }
+
+
+    @FXML
+    void RefreshTimeline1(ActionEvent event) throws IOException, InterruptedException {
+        comeToTimeline();
+    }
+
+
+    @FXML
+    void viewProfile1(ActionEvent event) throws IOException, InterruptedException {
+        viewProfile(event);
+    }
+
+
+    public void comeToTimeline() throws IOException, InterruptedException {
+        TimelineArea.getChildren().clear();
+        Socket socket = new Socket("127.0.0.1", 7600);
+        OutputStream out = socket.getOutputStream();
+        InputStream in = socket.getInputStream();
+        FileReader fileReader = new FileReader("C:\\Users\\ashkan mogharab\\Desktop\\clientGui\\index\\" + username + ".txt");
+        Scanner sc = new Scanner(fileReader);
+        String index = sc.next();
+        fileReader.close();
+        usefulmethods.send_message(out, "4");
+        Thread.sleep(100);
+        usefulmethods.send_message(out, index);
+        Thread.sleep(100);
+
+        int ownTweetsNumbers = Integer.parseInt(usefulmethods.read_message(in));
+        for (int i = 0; i < ownTweetsNumbers; i++) {
+            node node = new node();
+            node.getButton1().setText(usefulmethods.read_message(in));
+            node.getButton2().setText(usefulmethods.read_message(in) + " Retweets");
+            node.getButton3().setText(usefulmethods.read_message(in) + " likes");
+            node.getTextarea().setText(usefulmethods.read_message(in));
+            node.getLabel2().setText(usefulmethods.read_message(in));
+            Button deleteButton = new Button();
+            deleteButton.setPrefWidth(150);
+            deleteButton.setPrefHeight(27);
+            deleteButton.setText("delete");
+            deleteButton.setId("" + i);
+            deleteButton.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent actionEvent) {
+                    try {
+                        Socket so = new Socket("127.0.0.1", 7600);
+                        OutputStream ou = so.getOutputStream();
+                        String deletedTweetIndex = deleteButton.getId();
+                        usefulmethods.send_message(ou, "2");
+                        Thread.sleep(100);
+                        usefulmethods.send_message(ou, index);
+                        Thread.sleep(100);
+                        usefulmethods.send_message(ou, "2");
+                        Thread.sleep(100);
+                        usefulmethods.send_message(ou, deletedTweetIndex);
+                        Thread.sleep(100);
+                        comeToTimeline();
+                    } catch (InterruptedException | IOException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            });
+            node.getHBox1().getChildren().add(deleteButton);
+            TimelineArea.getChildren().add(node.asParent());
+        }
+        int favoriteUsersNumbers = Integer.parseInt(usefulmethods.read_message(in));
+        for (int i = 0; i < favoriteUsersNumbers; i++) {
+            int favoriteUserTweetNumbers = Integer.parseInt(usefulmethods.read_message(in));
+            for (int j = 0; j < favoriteUserTweetNumbers; j++) {
+                String index2 = Integer.toString(j);
+                node node = new node();
+                node.getLabel2().setText(usefulmethods.read_message(in));
+                node.getButton2().setText(usefulmethods.read_message(in) + " Retweets");
+                node.getButton3().setText(usefulmethods.read_message(in) + " likes");
+                node.getTextarea().setText(usefulmethods.read_message(in));
+                node.getButton1().setText(usefulmethods.read_message(in));
+                node.getButton1().setOnAction(new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent actionEvent) {
+                        processOfShowFavoriteUserProfile(actionEvent, node.getButton1());
+
+                    }
+                });
+                node.getButton2().setOnAction(new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent actionEvent) {
+                        processOfRetweet(node.getButton1(), node.getButton2(), index, index2);
+                    }
+                });
+                node.getButton3().setOnAction(new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent actionEvent) {
+                        processOfLike(node.getButton1(), node.getButton3(), index, index2);
+                    }
+                });
+                TimelineArea.getChildren().add(node.asParent());
+            }
+            int favoriteUserReTweetNumbers = Integer.parseInt(usefulmethods.read_message(in));
+            for (int k = 0; k < favoriteUserReTweetNumbers; k++) {
+                String index3 = Integer.toString(k);
+                node node = new node();
+                node.getLabel1().setText(usefulmethods.read_message(in) + " Retweeted ");
+                node.getButton1().setText(usefulmethods.read_message(in));
+                node.getLabel2().setText(usefulmethods.read_message(in));
+                node.getButton2().setText(usefulmethods.read_message(in) + " Retweets");
+                node.getButton3().setText(usefulmethods.read_message(in) + " likes");
+                node.getTextarea().setText(usefulmethods.read_message(in));
+                node.getButton3().setOnAction(new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent actionEvent) {
+                        processOfLike(node.getButton1(), node.getButton3(), index, index3);
+                    }
+                });
+                node.getButton2().setOnAction(new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent actionEvent) {
+                        processOfRetweet(node.getButton1(), node.getButton2(), index, index3);
+                    }
+                });
+                node.getButton1().setOnAction(new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent actionEvent) {
+                        processOfShowFavoriteUserProfile(actionEvent, node.getButton1());
+
+                    }
+                });
+                TimelineArea.getChildren().add(node.asParent());
+            }
+            int favoriteUserLikedNumbers = Integer.parseInt(usefulmethods.read_message(in));
+            for (int p = 0; p < favoriteUserLikedNumbers; p++) {
+                String index4 = Integer.toString(p);
+                node node = new node();
+                node.getButton2().setOnAction(new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent actionEvent) {
+                        processOfRetweet(node.getButton1(), node.getButton2(), index, index4);
+                    }
+                });
+                node.getLabel1().setText(usefulmethods.read_message(in) + " liked");
+                node.getButton1().setText(usefulmethods.read_message(in));
+                node.getLabel2().setText(usefulmethods.read_message(in));
+                node.getButton2().setText(usefulmethods.read_message(in) + " Retweets");
+                node.getButton3().setText(usefulmethods.read_message(in) + "likes");
+                node.getTextarea().setText(usefulmethods.read_message(in));
+                node.getButton3().setOnAction(new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent actionEvent) {
+                        processOfLike(node.getButton1(), node.getButton3(), index, index4);
+                    }
+                });
+                node.getButton1().setOnAction(new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent actionEvent) {
+                        processOfShowFavoriteUserProfile(actionEvent, node.getButton1());
+
+                    }
+                });
+                TimelineArea.getChildren().add(node.asParent());
+
+            }
+        }
+        socket.close();
+        in.close();
+        out.close();
+    }
+
+    private void processOfShowFavoriteUserProfile(ActionEvent actionEvent, Button button1) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("Profile-view2.fxml"));
+            Parent root = (Parent) loader.load();
+            profileView2Controller profileview2controller = loader.getController();
+            profileview2controller.setUsername(button1.getText(), username);
+            profileview2controller.comeToProfileView2Controller(1);
+            Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.setTitle("Profile");
+            stage.show();
+        } catch (IOException | InterruptedException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private void processOfRetweet(Button button1, Button button2, String index, String index2) {
+        try {
+            Socket so = new Socket("127.0.0.1", 7600);
+            OutputStream ou = so.getOutputStream();
+            InputStream inp = so.getInputStream();
+            usefulmethods.send_message(ou, "2");
+            Thread.sleep(100);
+            usefulmethods.send_message(ou, index);
+            Thread.sleep(100);
+            usefulmethods.send_message(ou, "1");
+            Thread.sleep(100);
+            usefulmethods.send_message(ou, "2");
+            Thread.sleep(100);
+            usefulmethods.send_message(ou, button1.getText());
+            Thread.sleep(100);
+            usefulmethods.send_message(ou, index2);
+            Thread.sleep(100);
+            if (usefulmethods.read_message(inp).equals("true")) {
+                button2.setText(usefulmethods.read_message(inp) + " Retweets");
+            }
+
+
+            so.close();
+            inp.close();
+            ou.close();
+            comeToTimeline();
+        } catch (InterruptedException | IOException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+
+    private void processOfLike(Button button1, Button button3, String index, String index2) {
+        try {
+            Socket so = new Socket("127.0.0.1", 7600);
+            OutputStream ou = so.getOutputStream();
+            InputStream inp = so.getInputStream();
+            usefulmethods.send_message(ou, "2");
+            Thread.sleep(101);
+            usefulmethods.send_message(ou, index);
+            Thread.sleep(101);
+            usefulmethods.send_message(ou, "3");
+            Thread.sleep(101);
+            usefulmethods.send_message(ou, button1.getText());
+            Thread.sleep(101);
+            usefulmethods.send_message(ou, index2);
+            Thread.sleep(101);
+            if (usefulmethods.read_message(inp).equals("true")) {
+                button3.setText(usefulmethods.read_message(inp) + "likes");
+            }
+
+
+            so.close();
+            inp.close();
+            ou.close();
+            comeToTimeline();
+        } catch (InterruptedException | IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private void showTweetScene(Event event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Tweet-view.fxml"));
+        Parent root = (Parent) loader.load();
+        TweetController tweetController = loader.getController();
+        tweetController.setUsername(username);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Tweet");
+        stage.show();
+    }
+
+    private void viewProfile(Event event) throws IOException, InterruptedException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Profile-view1.fxml"));
+        Parent root = (Parent) loader.load();
+        profileView1Controller profileview1controller = loader.getController();
+        profileview1controller.setUsername(username);
+        profileview1controller.comeToProfileView1Controller(1);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Profile");
+        stage.show();
+    }
+
+    private void viewSearch(Event event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("search-view.fxml"));
+        Parent root = (Parent) loader.load();
+        searchViewController searchviewcontroller = loader.getController();
+        searchviewcontroller.setUsername(username);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Search");
+        stage.show();
+    }
+
+}
