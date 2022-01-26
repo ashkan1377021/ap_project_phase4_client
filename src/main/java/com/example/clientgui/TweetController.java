1871951ae41371951784247f1583d77f924dad37
ashkan mogharab <ashkanmogharab77@aut.ac.ir>
2022-01-26 16:21:34 +0330

    TweetController class that controls Tweet-view.fxml added

---
+++ src/main/java/com/example/clientgui/TweetController.java
@@ -0,0 +1,95 @@
+package com.example.clientgui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.Scanner;
+
+public class TweetController implements Initializable {
+    usefulMethods usefulmethods = new usefulMethods();
+    String username;
+    @FXML
+    private TextArea TweetingArea;
+
+    @FXML
+    private Label resultMessage;
+
+    @FXML
+    void backToTimeline1(ActionEvent event) throws IOException, InterruptedException {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Timeline-view.fxml"));
+        Parent root = (Parent) loader.load();
+        TimelineController timelineController = loader.getController();
+        Scene scene = new Scene(root);
+        timelineController.setUsername(username);
+        timelineController.comeToTimeline();
+        stage.setScene(scene);
+        stage.setTitle("Timeline");
+        stage.show();
+    }
+
+    @FXML
+    void backToTimeline2(ActionEvent event) throws IOException, InterruptedException {
+
+        Socket socket = new Socket("127.0.0.1", 7600);
+        InputStream in = socket.getInputStream();
+        OutputStream out = socket.getOutputStream();
+        FileReader fileReader = new FileReader("C:\\Users\\ashkan mogharab\\Desktop\\clientGui\\index\\" + username + ".txt");
+        Scanner sc = new Scanner(fileReader);
+        String index = sc.next();
+        fileReader.close();
+        usefulmethods.send_message(out, "2");
+        Thread.sleep(100);
+        usefulmethods.send_message(out, index);
+        Thread.sleep(100);
+        usefulmethods.send_message(out, "1");
+        Thread.sleep(100);
+        usefulmethods.send_message(out, "1");
+        Thread.sleep(100);
+        usefulmethods.send_message(out, TweetingArea.getText());
+        Thread.sleep(100);
+        String st = usefulmethods.read_message(in);
+        if (st.equals("true")) {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("Timeline-view.fxml"));
+            Parent root = (Parent) loader.load();
+            TimelineController timelineController = loader.getController();
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            timelineController.setUsername(username);
+            timelineController.comeToTimeline();
+            stage.setScene(scene);
+            stage.setTitle("Timeline");
+            stage.show();
+        } else {
+            resultMessage.setText("Tweet can be up to 256 characters!");
+        }
+        socket.close();
+        in.close();
+        out.close();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+}
